3c8e460a54b8913af1836ec0aa1fca3b200c4f96
ppudding3861 <ppuding3861@gmail.com>
2024-04-11 18:01:28 +0900

    first commit

---
+++ chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/Application01.java
@@ -0,0 +1,27 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application01 {
+
+    public static void main(String[] args){
+        // 리터럴이란 값 자체를 의미한다.
+
+        // 정수형 리터럴
+        System.out.println(123);
+        // 실수형 리터럴
+        System.out.println(3.05);
+        // 문자형 리터럴
+        System.out.println('A');
+        // 문자열형 리터럴
+        System.out.println("hello world");
+
+        // "문자열형 리터럴"
+        System.out.println("12345");
+        System.out.println("123.45");
+
+        // 논리형 리터럴
+        System.out.println(true);
+        System.out.println(false);
+
+    }
+
+}
---
+++ chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/Application02.java
@@ -0,0 +1,42 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application02 {
+
+    public static void main(String[] args) {
+
+        // 2+2 화면에 출력
+        /* */
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2+2);
+        System.out.println(2/2);
+
+        // 123 % 10
+        System.out.println(123%10);
+
+        // 1.23 + 1.23
+        // 1.23 - 1.23
+        // 1.23 * 1.23
+        // 1.23 / 1.23
+        // 1.23 % 1.23
+        System.out.println("<2번째 사칙연산 계산 법칙>");
+        System.out.println(1.23 + 1.23);
+        System.out.println(1.23 - 1.23);
+        System.out.println(1.23 * 1.23);
+        System.out.println(1.23 / 1.23);
+        System.out.println(1.23 % 1.23);
+
+
+        // "123" + "12"
+        System.out.println("123" + "12");
+
+        // "123"-"12"
+       // System.out.println("123" - "12");
+        //"123"*"12"
+        // System.out.println("123" * "12");
+
+        System.out.println("123" + 12);
+
+    }
+
+}
